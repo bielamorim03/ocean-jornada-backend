a9eed32e7b3dfa3025f8ea4ed3856b1a0d85b5d0
Gabriel Amorim <152872047+bielamorim03@users.noreply.github.com>
2024-02-21 14:24:38 +0000

    Savepoint '/item' criado

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.send('Hello, World!')
 })
 
 
 app.get('/oi', function (req, res) {
     res.send('Olá Mundo!')
   })
+
+
+// Lista de Personagens
+const lista = ['Rick Sanchez', 'Morty Smith', 'Summer Smith']
+
+// Read all -> [GET] /item
+
+app.get('/item', function(req, res){
+  res.send(lista)
+})
+
 
 app.listen(3000)
